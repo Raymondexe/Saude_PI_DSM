76339ad05f98c7b54e91e29c7d9596b23014c61c
Pedro Henrique Raimundo <160554309+Raymondexe@users.noreply.github.com>
2026-06-12 09:31:48 -0300

    Update conexao.php

--- php/config/conexao.php
+++ php/config/conexao.php
@@ -1,9 +1,10 @@
 <?php
 
-$databaseUrl = getenv('DATABASE_URL');
+$url = "postgresql://neondb_owner:SUA_SENHA@ep-tiny-thunder-ahxteyhw-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require";
 
-$conn = pg_connect($databaseUrl . " sslmode=require");
+$conn = pg_connect($url);
 
 if (!$conn) {
     die("Erro na conexão com o banco.");
 }
+?>
